d2df32c42b3e58f69ed26176e24e02db37d08e02
Julia Moses <jslmoses@gmail.com>
2019-06-24 15:43:10 -0400

    carousel added on home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,6 +4,8 @@ import './Home.css';
 const Home = () =>
 {
   return (
+
+<div>
     <div>
     	<nav className="jumbotron">
     	<div className="theNav">
@@ -12,14 +14,38 @@ const Home = () =>
 	      	Get files <i>faster</i>
 	      </p>
 	      	<div><hr className="my-4"></hr></div>
+	      <p>A blahablah app that does neat thing take your files and go places feel good wow hooray</p>
         <a type="button" className="btn p-2 bd-highlight btn-primary btn-lg " href="" download="teamup.exe" >		
         	Download
         </a>
       </div>
-    	</nav>
-
-    	
+    	</nav>  	
     </div>
+
+     <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="../public/images/code-background.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+</div>
+    
+    
   )
 }
 

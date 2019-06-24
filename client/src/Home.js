1eaefb9f755e935d1f5aec039e91dfa731db6fb7
Julia Moses <jslmoses@gmail.com>
2019-06-24 04:48:31 -0400

    bootstrap instsalled- css file structure refactored

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () =>
 {
   return (
-    <div className="App">
-      <h1>Team Up!</h1>
+    <div className="jumbotron">
+      <h1 className="display-4"
+      >Team Up!</h1>
       <div>
-        <a href="" download="teamup.exe">Download</a>
+        <a type="button" className="btn btn-primary btn-lg active" href="" download="teamup.exe">
+        Download
+      	</a>
       </div>
     </div>
   )

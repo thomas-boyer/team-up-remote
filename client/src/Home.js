6a3ff48a892c93510a974f24e493c970462913ca
Julia Moses <jslmoses@gmail.com>
2019-06-30 03:47:26 -0400

    new nav bar

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,30 +6,25 @@ const Home = () =>
   return (
 <div>
   <div>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Team Up</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Upload</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Download</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Team Up</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Upload</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Download</a>
+    </li>
+  </ul>
   </div>
 
-  <div><a type="button" href="" download="teamup.exe">Download</a></div>
+  <div>
+    <button type="button" class="btn btn-secondary btn-lg" href="" download="teamup.exe">
+      Download
+    </button>
+  </div>
+
 </div>
 
   )

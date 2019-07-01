22e83eb7b6bb3ad273c756f895f560b90d28e9ec
Julia Moses <jslmoses@gmail.com>
2019-07-01 17:50:10 -0400

    nav bar fixed

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,26 +4,30 @@ import React from 'react';
 const Home = () =>
 {
   return (
-<div>
+<div className="home">
   <div>
-  <ul class="nav">
-    <li class="nav-item">
-      <a class="nav-link active" href="#">Team Up</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Upload</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Download</a>
-    </li>
-  </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">Team Up</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Upload</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Download</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   </div>
 
-  <div>
-    <button type="button" class="btn btn-secondary btn-lg" href="" download="teamup.exe">
-      Download
-    </button>
-  </div>
+  <div><a type="button" href="" download="teamup.exe">Download</a></div>
 
 </div>
 

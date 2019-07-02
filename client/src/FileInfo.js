b6f49c5a589773e11e71e71fbdbf1bc78e4895fe
Julia Moses <jslmoses@gmail.com>
2019-07-02 03:31:42 -0400

    styling on hcunks

--- client/src/FileInfo.js
+++ client/src/FileInfo.js
@@ -14,13 +14,37 @@ const FileInfo = ({ file, userEmail, socket }) =>
   }
 
   return (
-    <div className="file-info">
-      <h1>{ file.file_name }</h1>
-
+    <div>
       <div>
-        <ProgressBar data={ file }/>
-        <h2>Upload completing</h2>
-        { getChunks(file) }
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="">Team Up</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">Upload</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">Download</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      </div>
+
+      <div className="file-info">
+        <h1>{ file.file_name }</h1>
+
+        <div>
+          <ProgressBar data={ file }/>
+          <h2>Upload completing</h2>
+          { getChunks(file) }
+        </div>
       </div>
     </div>
   )
